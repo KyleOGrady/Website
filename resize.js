b721ce42daef9df33133d74f3eb5a44f1bcd538a
KyleOGrady <ogradyks@clarkson.edu>
2017-10-25 17:47:13 -0400

    Add files via upload

---
+++ resize.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+
+var padding = document.getElementById("center_image");
+var style = window.getComputedStyle(padding);
+var padding = style.getPropertyValue('padding-top');
+
+var newSize = 20;
+
+window.onresize = function(){
+	var windowSize = $(window).width();
+
+	if(windowSize < 1000){
+	
+		newSize--;
+		document.getElementById("center_image").style.paddingTop = newSize;	
+	} 
+	
+	if(windowSize >= 1000){
+		newSize = 20;
+		document.getElementById("center_image").style.paddingTop = newSize;
+	}
+	
+	
+}
