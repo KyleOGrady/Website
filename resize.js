4b644294545528af255570c7d1d14d3419d28461
Kyle O'Gradya <ogradyks@clarkson.edu>
2017-10-26 12:17:14 -0400

    Fixed footer size issue

--- resize.js
+++ resize.js
@@ -10,16 +10,15 @@ var newSize = 20;
 window.onresize = function(){
 	var windowSize = $(window).width();
 
+	if(windowSize >= 1000){
+		newSize = 20;
+		document.getElementById("center_image").style.paddingTop = newSize;
+	}
+
 	if(windowSize < 1000){
 	
 		newSize--;
 		document.getElementById("center_image").style.paddingTop = newSize;	
 	} 
-	
-	if(windowSize >= 1000){
-		newSize = 20;
-		document.getElementById("center_image").style.paddingTop = newSize;
-	}
-	
-	
+
 }
